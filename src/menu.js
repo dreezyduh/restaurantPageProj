29602151403be63246422e0321c61573e8b2b5d5
VLAD <19872650+dreezyduh@users.noreply.github.com>
2024-04-09 23:55:48 +0300

    WIP

--- src/menu.js
+++ src/menu.js
@@ -21,91 +21,91 @@ function menu() {
         pizzaQuatro: {
             name: 'Pizza Quattro Stagioni',
             ingredients: 'Pizza de baza + pepperoni, ciuperci, masline, ardei',
-            image: '/images/pizza/quattrostagioni.png'
+            image: './images/pizza/quattrostagioni.png'
         },
 
         pizzaQuatro2: {
             name: 'Pizza Quattro Stagioni Amestecat',
             ingredients: 'Pizza de baza + pepperoni, ciuperci, masline, ardei',
-            image: '../images/pizza/quattrostagionimix.png'
+            image: './images/pizza/quattrostagionimix.png'
         },
 
         pizzaMargherita: {
             name: 'Pizza Margherita',
             ingredients: 'Pizza de baza + mozzarella, busuioc',
-            image: '../images/pizza/margherita.png'
+            image: './images/pizza/margherita.png'
         },
 
         pizzaDiavola: {
             name: 'Pizza Diavola',
             ingredients: 'Pizza de baza + salam picant',
-            image: '../images/pizza/diavola.png'
+            image: './images/pizza/diavola.png'
         },
         
         pizzaConTonno: {
             name: 'Pizza Con Tonno',
             ingredients: 'Pizza de baza + ton, porumb, ciuperci, masline',
-            image: '../images/pizza/tonno.png'
+            image: './images/pizza/tonno.png'
         },
 
         pizzaCarnivora: {
             name: 'Pizza Carnivora',
             ingredients: 'Pizza de baza + sunca, pepperoni, pulled pork, pui',
-            image: '../images/pizza/carnivora.png'
+            image: './images/pizza/carnivora.png'
         },
 
         pizzaCapriciosa: {
             name: 'Pizza Capriciosa',
             ingredients: 'Pizza de baza + ciuperci, sunca, ardei, masline',
-            image: '../images/pizza/capriciosa.png'
+            image: './images/pizza/capriciosa.png'
         },
 
         pizzaProsciutto: {
             name: 'Pizza Prosciutto E Funghi',
             ingredients: 'Pizza de baza + ciuperci, sunca',
-            image: '../images/pizza/prosciutto.png'
+            image: './images/pizza/prosciutto.png'
         },
 
         pizzaAllcheese: {
             name: 'Pizza All Cheese',
             ingredients: 'Pizza de baza + gorgonzola, mozzarella, cedar, camembert, crema de branza',
-            image: '../images/pizza/allcheese.png'
+            image: './images/pizza/allcheese.png'
         },
 
         pizzaCountry: {
             name: 'Pizza Country',
             ingredients: 'Pizza de baza + bucati bacon, porumb, ceapa, ciuperci',
-            image: '../images/pizza/country.png'
+            image: './images/pizza/country.png'
         },
 
         pizzaAloha: {
             name: 'Pizza Aloha',
             ingredients: 'Pizza de baza + sunca, ananas, porumb, ceapa',
-            image: '../images/pizza/aloha.png'
+            image: './images/pizza/aloha.png'
         },
 
         pizzaMexico: {
             name: 'Pizza Mexico',
             ingredients: 'Pizza de baza + sos chilly, pepperoni, porumb, ceapa, jalapenos, nachos',
-            image: '../images/pizza/mexico.png'
+            image: './images/pizza/mexico.png'
         },
 
         pizzaBBQ: {
             name: 'Pizza BBQ',
             ingredients: 'Pizza de baza + sos BBQ, ceapa, porumb, bucati bacon, ciuperci, pui',
-            image: '../images/pizza/bbq.png'
+            image: './images/pizza/bbq.png'
         },
 
         pizzaProvence: {
             name: 'Pizza Provence',
             ingredients: 'Pizza de baza + blue cheese, rosii uscate, pui, ceapa, ardei',
-            image: '../images/pizza/provence.png'
+            image: './images/pizza/provence.png'
         },
 
         pizzaArgentina: {
             name: 'Pizza Argentina',
             ingredients: 'Pizza de baza + carne tocata, ardei, branza cedar, ceapa',
-            image: '../images/pizza/argentina.png'
+            image: './images/pizza/argentina.png'
         }
     }
     
@@ -114,199 +114,199 @@ function menu() {
         pizzaSauce:  {
             name: 'sos de pizza',
             id: 'pizzaSauce',
-            image: '../images/toppings/pizzasauce.png'
+            image: './images/toppings/pizzasauce.png'
         },
 
         bbqSauce: {
             name: 'sos BBQ',
             id: 'bbqSauce',
-            image: '../images/toppings/bbq.png'
+            image: './images/toppings/bbq.png'
         },
 
         bbqMixSauce: {
             name: 'sos BBQ cu sos Pizza',
             id: 'bbqMixSauce',
-            image: '../images/toppings/bbqsauce.png'
+            image: './images/toppings/bbqsauce.png'
         },
 
         sweetChilly:  {
             name: 'sos sweet chilly',
             id: 'sweetChilly',
-            image: '../images/toppings/sweetchilly.png'
+            image: './images/toppings/sweetchilly.png'
         },
 
         spicyGum:  {
             name: 'sos balon',
             id: 'spicyGum',
-            image: '../images/toppings/spicygum.png'
+            image: './images/toppings/spicygum.png'
         },
 
         ananas: {
             name: 'Ananas',
             id: 'ananas',
-            image: '../images/toppings/ananas.png'
+            image: './images/toppings/ananas.png'
         },
 
         bacon: {
             name: 'Bacon',
             id: 'bacon',
-            image: '../images/toppings/bacon.png'
+            image: './images/toppings/bacon.png'
         },
 
         banana: {
             name: 'Banana',
             id: 'banana',
-            image: '../images/toppings/banana.png'
+            image: './images/toppings/banana.png'
         },
 
         camembert: {
             name: 'Branza Camembert',
             id: 'camembert',
-            image: '../images/toppings/camembert.png'
+            image: './images/toppings/camembert.png'
         },
 
         cheddar: {
             name: 'Branza Cedar',
             id: 'cheddar',
-            image: '../images/toppings/cheddar.png'
+            image: './images/toppings/cheddar.png'
         },
 
         chicken: {
             name: 'Pui',
             id: 'chicken',
-            image: '../images/toppings/chicken.png'
+            image: './images/toppings/chicken.png'
         },
 
         chiliflakes: {
             name: 'Fulgi de Chili',
             id: 'chiliflakes',
-            image: '../images/toppings/chiliflakes.png'
+            image: './images/toppings/chiliflakes.png'
         },
 
         corn: {
             name: 'Porumb',
             id: 'corn',
-            image: '../images/toppings/corn.png'
+            image: './images/toppings/corn.png'
         },
 
         creamCheese: {
             name: 'Crema de Branza',
             id: 'creamCheese',
-            image: '../images/toppings/creamCheese.png'
+            image: './images/toppings/creamCheese.png'
         },
 
         gradeCheese: {
             name: 'Branza cu Mucegai',
             id: 'gradeCheese',
-            image: '../images/toppings/gradeCheese.png'
+            image: './images/toppings/gradeCheese.png'
         },
 
         ham: {
             name: 'Sunca',
             id: 'ham',
-            image: '../images/toppings/ham.png'
+            image: './images/toppings/ham.png'
         },
 
         jalapeno: {
             name: 'Jalapeno',
             id: 'jalapeno',
-            image: '../images/toppings/jalapeno.png'
+            image: './images/toppings/jalapeno.png'
         },
 
         mincedmeat: {
             name: 'Carne tocata',
             id: 'mincedmeat',
-            image: '../images/toppings/mincedmeat.png'
+            image: './images/toppings/mincedmeat.png'
         },
 
         mozzarella: {
             name: 'Mozzarella',
             id: 'mozzarella',
-            image: '../images/toppings/mozzarella.png'
+            image: './images/toppings/mozzarella.png'
         },
 
         mushroom: {
             name: 'Ciuperci',
             id: 'mushroom',
-            image: '../images/toppings/mushroom.png'
+            image: './images/toppings/mushroom.png'
         },
 
         nachos: {
             name: 'Nachos',
             id: 'nachos',
-            image: '../images/toppings/nachos.png'
+            image: './images/toppings/nachos.png'
         },
 
         olives: {
             name: 'Masline',
             id: 'olives',
-            image: '../images/toppings/olives.png'
+            image: './images/toppings/olives.png'
         },
 
         onion: {
             name: 'Ceapa',
             id: 'onion',
-            image: '../images/toppings/onion.png'
+            image: './images/toppings/onion.png'
         },
 
         oregano: {
             name: 'Oregano',
             id: 'oregano',
-            image: '../images/toppings/oregano.png'
+            image: './images/toppings/oregano.png'
         },
 
         paprika: {
             name: 'Ardei gras',
             id: 'paprika',
-            image: '../images/toppings/paprika.png'
+            image: './images/toppings/paprika.png'
         },
 
         pepper: {
             name: 'Piper',
             id: 'pepper',
-            image: '../images/toppings/pepper.png'
+            image: './images/toppings/pepper.png'
         },
 
         pepperoni: {
             name: 'Pepperoni',
             id: 'pepperoni',
-            image: '../images/toppings/pepperoni.png'
+            image: './images/toppings/pepperoni.png'
         },
 
         pulledPork: {
             name: 'Pulled Pork',
             id: 'pulledPork',
-            image: '../images/toppings/pulledpork.png'
+            image: './images/toppings/pulledpork.png'
         },
 
         redOnion: {
             name: 'Ceapa Rosie',
             id: 'redOnion',
-            image: '../images/toppings/redonion.png'
+            image: './images/toppings/redonion.png'
         },
 
         saltyTomato: {
             name: 'Rosii Uscate',
             id: 'saltyTomato',
-            image: '../images/toppings/saltytomato.png'
+            image: './images/toppings/saltytomato.png'
         },
 
         spinach: {
             name: 'Spanac',
             id: 'spinach',
-            image: '../images/toppings/spinach.png'
+            image: './images/toppings/spinach.png'
         },
 
         tomato: {
             name: 'Rosii',
             id: 'tomato',
-            image: '../images/toppings/tomato.png'
+            image: './images/toppings/tomato.png'
         },
 
         tuna: {
             name: 'Ton',
             id: 'tuna',
-            image: '../images/toppings/tuna.png'
+            image: './images/toppings/tuna.png'
         }
     }
 
@@ -380,7 +380,7 @@ function menu() {
         makeYourPizzaTitle.textContent = 'Creeaza pizza ta!';
         makeYourPizzaContainer.textContent = 'Topping-uri:';
         makeYourPizzaImgContainer.textContent = '';
-        makeYourPizzaImg.src = '../images/pizza/emptypizza.png';
+        makeYourPizzaImg.src = './images/pizza/emptypizza.png';
         makeYourPizzaImg.width = '350';
 
         makeYourPizzaImgContainer.setAttribute('class', 'makeYourPizzaImgContainer');
